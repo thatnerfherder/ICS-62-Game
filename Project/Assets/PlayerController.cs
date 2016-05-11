7f76db3b544b9410a16720a1440cf3e15afa1650
thatnerfherder <jlamorte@uci.edu>
2016-05-10 20:33:37 -0700

    Scripts 1.0
    edited script so that the cube shrinks slowly. Added script for picking
    up objects.

---
+++ Project/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+	
+	}
+	
+	// Update is called once per frame
+
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+    void Update()
+    {
+        if(transform.localScale.x > 0.5)
+            transform.localScale += new Vector3(-0.001f, -0.001f, -0.001f);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Capsule"))
+        {
+            Destroy(other.gameObject);
+            transform.localScale += new Vector3(.1f, .1f, .1f);
+        }
+    }
+
+
+   
+}
